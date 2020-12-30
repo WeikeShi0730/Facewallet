aee0fad6593a7a629c203882058f991c18a84ba8
Weike Shi <shiw14@mcmaster.ca>
2020-12-30 18:41:53 -0500

    set up demo flask backend framework

--- flask-backend-demo/main.py
+++ flask-backend-demo/main.py
@@ -1,18 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, request, json
+import time
 
 app = Flask("__main__")
 
 
-@app.route("/")
-def my_index():
-    return render_template("index.html", token="flask-react-demo")
-
-@app.route("/register")
+@app.route("/register", methods=['POST'])
 def register():
-    return 
+    data = json.loads(request.data, strict=False)
+    if "photo" in data:
+        register_photo(data)
+    else:
+        register_info(data)
+    return data
+
+
+def register_info(info_data):
+    time.sleep(1)
+    return info_data, 201
+
+
+def register_photo(photo_data):
+    time.sleep(5)
+    return photo_data, 201
+
+
+@app.route("/register", methods=['GET'])
+def register_message():
+    info = True
+    photo = True
+    message = "ok" if (info and photo) else "nok"
+    return jsonify({'message': message})
+
 
 @app.route("/payment")
 def payment():
-    return
-
-#app.run(debug=True)
+    return "payment"
