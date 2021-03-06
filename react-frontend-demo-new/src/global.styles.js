bfdeb4c509ce9fe6656f37422512afebf1ad7187
Weike Shi <shiw14@mcmaster.ca>
2021-03-05 22:17:36 -0500

    added temp background image

--- react-frontend-demo-new/src/global.styles.js
+++ react-frontend-demo-new/src/global.styles.js
@@ -3,7 +3,6 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 body {
     font-family: "formula1-display-regular";
-    padding: 20px 40px;
 
     @media screen and (max-width: 1200px) {
         padding: 10px;
