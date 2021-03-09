62a92ce411ebf20262970ad26415c678513267fa
Weike Shi <shiw14@mcmaster.ca>
2021-03-09 16:46:52 -0500

    added secondary check modal

--- react-frontend-demo-new/src/components/payment/payment.component.jsx
+++ react-frontend-demo-new/src/components/payment/payment.component.jsx
@@ -10,13 +10,20 @@ import "./payment.styles.scss";
 import WebcamWindow from "../camera-window/camera-window.component";
 import CustomButton from "../custom-buttom/custom-button.component";
 import FormInput from "../form-input/form-input.component";
+import CustomModal from "../modal/modal.component";
 
 function Payment({ amount, isLoading, currentUser, setAmount, setIsLoading }) {
   const { addToast } = useToasts();
-  const [wobble, setWobble] = useState(0);
+  const [checkmark, setCheckmark] = useState(0);
+  const [show, setShow] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState();
   const handleChange = (event) => {
-    const { value } = event.target;
-    setAmount(value);
+    const { name, value } = event.target;
+    if (name === "phone_number") {
+      setPhoneNumber(value);
+    } else if (name === "total") {
+      setAmount(value);
+    }
   };
 
   // webcam
@@ -48,15 +55,67 @@ function Payment({ amount, isLoading, currentUser, setAmount, setIsLoading }) {
           if (json.message === "succeed") {
             console.log("payment success!", json);
             // Checkmark animation timeout
-            setWobble(1);
+            setCheckmark(1);
             setTimeout(() => {
-              setWobble(0);
+              setCheckmark(0);
             }, 2500);
+          } else if (json.message === "Secondary verification needed") {
+            console.log("need seocndary verification");
+            setShow(true);
           }
           addToast(json.message + " " + json.person_id, {
             appearance: json.level,
             autoDismiss: true,
           });
+          return;
+        } catch (error) {
+          console.log(error);
+          addToast(error.message, {
+            appearance: json.level,
+            autoDismiss: true,
+          });
+        }
+      }
+    }
+  };
+
+  const handleSubmitSecondary = async (event) => {
+    event.preventDefault();
+    if (
+      phoneNumber !== undefined &&
+      phoneNumber !== null &&
+      phoneNumber !== ""
+    ) {
+      setIsLoading(true);
+      const response = await fetch(
+        `${process.env.REACT_APP_BACKEND_URL}/api/merchant/${currentUser.personId}/facepay/secondary`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ phone_number: phoneNumber }),
+        }
+      );
+      setIsLoading(false);
+
+      if (response.ok) {
+        const json = await response.json();
+        try {
+          if (json.message === "succeed") {
+            console.log("payment success!", json);
+            // Checkmark animation timeout
+            setCheckmark(1);
+            setTimeout(() => {
+              setCheckmark(0);
+            }, 2500);
+            setShow(false);
+          }
+          addToast(json.message + " " + json.person_id, {
+            appearance: json.level,
+            autoDismiss: true,
+          });
+          return;
         } catch (error) {
           console.log(error);
           addToast(error.message, {
@@ -77,26 +136,45 @@ function Payment({ amount, isLoading, currentUser, setAmount, setIsLoading }) {
         <div></div>
       </div>
 
-      <div className={`${wobble === 1 ? "mark" : ""}`}>
+      <div className={`${checkmark === 1 ? "mark" : ""}`}>
         <svg
-          className={`${wobble === 1 ? "checkmark" : "uncheckmark"}`}
+          className={`${checkmark === 1 ? "checkmark" : "uncheckmark"}`}
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 52 52"
         >
           <circle
-            className={`${wobble === 1 ? "checkmark__circle" : ""}`}
+            className={`${checkmark === 1 ? "checkmark__circle" : ""}`}
             cx="50%"
             cy="50%"
             r="25"
             fill="none"
           />
           <path
-            className={`${wobble === 1 ? "checkmark__check" : ""}`}
+            className={`${checkmark === 1 ? "checkmark__check" : ""}`}
             fill="none"
             d="M14.1 27.2l7.1 7.2 16.7-16.8"
           />
         </svg>
       </div>
+
+      <CustomModal
+        title="Secondary Verification"
+        onClose={() => setShow(false)}
+        show={show}
+      >
+        <label>Please Enter Phone Number</label>
+        <FormInput
+          name="phone_number"
+          type="text"
+          handleChange={handleSubmitSecondary}
+          value={phoneNumber}
+          label="Phone Number"
+          pattern="\d*"
+          minLength="10"
+          maxLength="11"
+          required
+        />
+      </CustomModal>
 
       <div className="group">
         <WebcamWindow ref={webcamRef} />
