841e89fa26829d1e54524d03ed261524120660fd
Weike Shi <shiw14@mcmaster.ca>
2021-03-20 23:37:47 -0400

    fix prfile page error

--- react-frontend-demo-new/src/pages/profile-customer/profile-customer.component.jsx
+++ react-frontend-demo-new/src/pages/profile-customer/profile-customer.component.jsx
@@ -1,20 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
+import { useToasts } from "react-toast-notifications";
 
 //import "./hompage.styles.scss";
 
 const ProfileCustomer = ({ currentUser }) => {
+  const { addToast } = useToasts();
+  const { transactions, setTransactions } = useState();
   const signedIn = currentUser !== null && currentUser.type === "customer";
 
-  const transactions = currentUser.Transactions.map((transaction) => ({
-    shop: transaction.Merchant.shop_name,
-    amount: transaction.amount,
-    time: transaction.data_time,
-  }));
+  useEffect(() => {
+    handleSubmit();
+  }, []);
 
-  return (
-    <div>{signedIn ? <div>{currentUser.personId}</div> : <div></div>}</div>
-  );
+  const handleSubmit = async (event) => {
+    const response = await fetch(
+      `${process.env.REACT_APP_BACKEND_URL}/api/customer/${currentUser.personId}/profile`,
+      {
+        method: "GET",
+      }
+    );
+    const json = await response.json();
+    try {
+      const personId = json.person_id;
+      if (personId === undefined) {
+        console.log(json);
+        // addToast(json.message, {
+        //   appearance: json.level,
+        //   autoDismiss: true,
+        // });
+      } else {
+        const transactions = currentUser.Transactions.map((transaction) => ({
+          shop: transaction.Merchant.shop_name,
+          amount: transaction.amount,
+          time: transaction.data_time,
+          balance: transaction.balance,
+        }));
+        console.log(transactions);
+        setTransactions(transactions);
+      }
+    } catch (error) {
+      // addToast(error, {
+      //   appearance: "error",
+      //   autoDismiss: true,
+      // });
+      console.log("User not found", error);
+    }
+  };
+
+  return <div>{signedIn ? <div>{transactions}</div> : <div></div>}</div>;
 };
 
 const mapStateToProps = (state) => ({
